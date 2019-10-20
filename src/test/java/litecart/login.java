c890b5c969ec0ec20b7ca9120cbb2eeb7931ea67
cheremnykhes <evgenia.cheremnyh@gmail.com>
2019-10-20 23:39:21 +0700

    Create login.java

---
+++ src/test/java/litecart/login.java
@@ -0,0 +1,31 @@
+package litecart;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class login {
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void login() {
+        driver.get("http://localhost:8080/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver=null;
+    }
+}
