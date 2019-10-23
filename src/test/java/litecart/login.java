2c15b76e9d9f7b60a39621fa81acc87fd8946d00
cheremnykhes <evgenia.cheremnyh@gmail.com>
2019-10-23 23:42:37 +0700

    Update login.java

--- src/test/java/litecart/login.java
+++ src/test/java/litecart/login.java
@@ -6,13 +6,25 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.io.File;
 
 public class login {
     private WebDriver driver;
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+
+       driver = new ChromeDriver();
+       // driver = new FirefoxDriver();
+        //driver = new SafariDriver();
+       // FirefoxOptions options = new FirefoxOptions();
+       // options.setBinary(new FirefoxBinary(new File("/Users/Evgenia/Applications/Firefox_Nightly.app/Contents/MacOS/firefox")));
+       // WebDriver driver = new FirefoxDriver(options);
     }
 
     @Test
