0b6f92c88fd2501e9a4f5b4aa9940ed5893c7563
cheremnykhes <evgenia.cheremnyh@gmail.com>
2019-11-01 00:06:26 +0700

    Update sortCountry.java

--- src/test/java/litecart/sortCountry.java
+++ src/test/java/litecart/sortCountry.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -16,38 +17,56 @@ public class sortCountry extends baseTest {
     @Before
     public void start() throws Exception {
         this.init();
-        driver.get("http://localhost/litecart/admin/login.php");
+        driver.get("http://localhost:8080/litecart/admin/login.php");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
     }
 
     @Test
-    public void login() throws Exception{
+    public void sortCountriesMain() throws Exception{
 
-        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+        List<String> name = new ArrayList<>();
+        List<String> sortName = new ArrayList<>();
+        List<String> href = new ArrayList<>();
 
+        driver.get("http://localhost:8080/litecart/admin/?app=countries&doc=countries");
 
         List<WebElement> countries = driver.findElements(By.cssSelector(".row"));
-        List<String> name = new ArrayList<>();
-        List<String> href = new ArrayList<>();
+
        for(int i=0; i<countries.size(); i++) {
             name.add(countries.get(i).findElement(By.xpath("./td[5]")).getText());
+            sortName.add(name.get(i));
+       }
 
-          //  zones.add(countries.get(i).findElement(By.xpath("./td[6]")).getText());
-        //    if (countries.get(i).findElement(By.xpath("./td[6]")).getText().equals("0")) {
-           //     href.add(countries.get(i).findElement(By.xpath("./td[5]")).getAttribute("href"));
-            }
-      //    assertEquals(1, products.get(i).findElements(By.cssSelector(".sticker")).size());
-        }
+            Collections.sort(name);
+        assertEquals(name,sortName);
 
     }
+
+        @Test
+        public void sortZones() throws Exception {
+
+            List<String> href = new ArrayList<>();
+
+            driver.get("http://localhost:8080/litecart/admin/?app=countries&doc=countries");
+
+            List<WebElement> countries = driver.findElements(By.cssSelector(".row"));
+
+            for (int i = 0;i<countries.size(); i++) {
+                if (!countries.get(i).findElement(By.xpath("./td[6]")).getText().equals("0")) {
+                    href.add(countries.get(i).findElement(By.xpath("./td[5]/a")).getAttribute("href"));
+                }
+
+                for(int j=0;j<href.size();j++) {
+                    driver.get(href.get(j));
+                }
+
+            }
+        }
 
     @After
     public void tearDown() throws Exception {
     }
-
-
-
 
 }
