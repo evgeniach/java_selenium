d659aa4be59b90433f4fa4fcc4ed6295af2cdc3f
cheremnykhes <evgenia.cheremnyh@gmail.com>
2019-11-03 16:15:50 +0700

    Update sortCountry.java

--- src/test/java/litecart/sortCountry.java
+++ src/test/java/litecart/sortCountry.java
@@ -1,5 +1,6 @@
 package litecart;
 
+import org.checkerframework.checker.units.qual.C;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +15,7 @@ import static org.junit.Assert.assertEquals;
 
 public class sortCountry extends baseTest {
 
+
     @Before
     public void start() throws Exception {
         this.init();
@@ -26,47 +28,103 @@ public class sortCountry extends baseTest {
     @Test
     public void sortCountriesMain() throws Exception{
 
-        List<String> name = new ArrayList<>();
-        List<String> sortName = new ArrayList<>();
-        List<String> href = new ArrayList<>();
+        List<String> nameCounties = new ArrayList<>();
+        List<WebElement> countries = new ArrayList<>();
 
         driver.get("http://localhost:8080/litecart/admin/?app=countries&doc=countries");
 
-        List<WebElement> countries = driver.findElements(By.cssSelector(".row"));
+        countries = driver.findElements(By.cssSelector(".row td:nth-child(5) a"));
 
-       for(int i=0; i<countries.size(); i++) {
-            name.add(countries.get(i).findElement(By.xpath("./td[5]")).getText());
-            sortName.add(name.get(i));
-       }
+        nameCounties=getText((ArrayList<WebElement>) countries);
 
-            Collections.sort(name);
-        assertEquals(name,sortName);
-
+        isSortByAlfabet((ArrayList) nameCounties);
     }
 
         @Test
         public void sortZones() throws Exception {
-
+            List<WebElement> zones = new ArrayList<>();
+            List<String> nameZone = new ArrayList<>();
+            List<WebElement> countries;
             List<String> href = new ArrayList<>();
 
             driver.get("http://localhost:8080/litecart/admin/?app=countries&doc=countries");
 
-            List<WebElement> countries = driver.findElements(By.cssSelector(".row"));
+            countries = driver.findElements(By.cssSelector(".row"));
+
 
             for (int i = 0;i<countries.size(); i++) {
                 if (!countries.get(i).findElement(By.xpath("./td[6]")).getText().equals("0")) {
                     href.add(countries.get(i).findElement(By.xpath("./td[5]/a")).getAttribute("href"));
                 }
+            }
 
-                for(int j=0;j<href.size();j++) {
-                    driver.get(href.get(j));
-                }
+            for(int j=0;j<href.size();j++) {
+                driver.get(href.get(j));
+
+                nameZone.clear();
+                zones.clear();
+
+                zones = driver.findElements(By.cssSelector("table#table-zones td:nth-child(3)"));
+                nameZone=getText((ArrayList<WebElement>) zones);
+
+                isSortByAlfabet((ArrayList) nameZone);
 
             }
         }
+
+    @Test
+    public void sortGeoZones() throws Exception {
+        List<String> href = new ArrayList<>();
+        List<String> nameZone = new ArrayList<>();
+        List<WebElement> zones =  new ArrayList<>();
+
+        driver.get("http://localhost:8080/litecart/admin/?app=geo_zones&doc=geo_zones");
+
+        List<WebElement> geoZones = driver.findElements(By.cssSelector(".row td:nth-child(3) a"));
+
+        for(int i=0;i<geoZones.size();i++) {
+            href.add(geoZones.get(i).getAttribute("href"));
+        }
+
+        for(int j=0;j<href.size();j++) {
+
+            nameZone.clear();
+            zones.clear();
+
+            driver.get(href.get(j));
+            zones = driver.findElements(By.cssSelector("select[name *=zone_code] > option[selected]"));
+
+            nameZone=getText((ArrayList<WebElement>) zones);
+
+            isSortByAlfabet((ArrayList) nameZone);
+
+        }
+    }
+
+
 
     @After
     public void tearDown() throws Exception {
     }
 
+    public void isSortByAlfabet(ArrayList sequence){
+        List<String> sortSequence = new ArrayList<>();
+        sortSequence.addAll(sequence);
+        Collections.sort(sortSequence);
+        assertEquals(sortSequence,sequence);;
+    }
+
+    
+    public ArrayList getText(ArrayList<WebElement> pageElements) {
+        List<String> gettText = new ArrayList<>();
+
+        for(int i=0;i<pageElements.size()-1;i++) {
+            gettText.add(pageElements.get(i).getText());
+        }
+        return (ArrayList) gettText;
+    }
+
+
+
 }
+
